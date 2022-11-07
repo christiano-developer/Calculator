777379559597a84b90e1972cf72e3d1f3fa6bfc5
christiano-developer <88627133+christiano-developer@users.noreply.github.com>
2022-11-07 20:52:37 +0530

    more js

--- main/main.js
+++ main/main.js
@@ -1,11 +1,29 @@
+let btn = document.getElementsByClassName("btn")
+let clear = document.getElementById("_clear")
+let textArea = document.getElementById("_textarea")
+
+const arroFcals = [
+  "(", ")", "%", "AC", "7", "8", "9", "÷", "4", "5", "6", "×", "1", "2", "3", "−", "0", ".", "=", "+"
+]
 
 
-let mep = document.getElementById("mep")
-let bnk = document.getElementById("myBtn")
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', function () {
 
-bnk.addEventListener("click", function() {
-  mep.textContent = "(";
+    console.log(btn[i].textContent)
+    textArea.textContent += btn[i].textContent
+    document.getElementById("_textarea").style.color = "black";
+  });
+}
 
-});
+
+
+clear.addEventListener("click", function(){
+
+  textArea.textContent = ""
+  document.getElementById("_textarea").style.color = "rgba(163, 168, 114, 0.5)";
+})
+
+
 
 
