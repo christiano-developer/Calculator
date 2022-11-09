af1d47b23a7b2b5bd76c1fed267ee4928a0bfdc3
christiano-developer <88627133+christiano-developer@users.noreply.github.com>
2022-11-09 22:52:32 +0530

    for the day 11pm

--- main/main.js
+++ main/main.js
@@ -10,20 +10,39 @@ const arroFcals = [
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener('click', function () {
 
-    //console.log(btn[i].textContent)
-    
+    console.log(btn[i].textContent)
+    console.log(ggb)
     textArea.textContent += btn[i].textContent
     document.getElementById("_textarea").style.color = "black";
+    textArea.textContent = truncateText('p', 31)
   });
 }
 
 
-
+//All Clear
 clear.addEventListener("click", function(){
 
   textArea.textContent = ""
   document.getElementById("_textarea").style.color = "rgba(163, 168, 114, 0.5)";
 })
+
+
+
+//to stop characters more than 31
+function truncateText(p, maxLength) {
+  var element = document.querySelector("#_textarea"),
+      truncated = element.innerText;
+
+  if (truncated.length > maxLength) {
+      truncated = truncated.substring(0,maxLength);
+  }
+  return truncated;
+}
+
+
+
+let gga = document.getElementById("test")
+let ggb = gga.getAttribute("value")
 
 
 
